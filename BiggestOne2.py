63938f13ceae465b1641d32fd9dfcbe19c0f5474
trytek235 <bibn@wp.pl>
2023-02-27 11:33:36 +0100

    x:1

---
+++ BiggestOne2.py
@@ -0,0 +1,8 @@
+def max(list):
+    if len(list) == 2:
+        return list[0] if list[0] > list[1] else list[1]
+    sub_max = max(list[1:])
+    return list[0] if list[0] > sub_max else sub_max
+
+array = [555,4,5,243,43]
+print(max(array))
