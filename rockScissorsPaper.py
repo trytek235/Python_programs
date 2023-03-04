db1a8c017a863fe9838cd6633bc0dca628441881
trytek235 <bibn@wp.pl>
2023-03-04 20:00:41 +0100

    Version: 2

--- rockScissorsPaper.py
+++ rockScissorsPaper.py
@@ -1,11 +1,11 @@
 import random
 import time
 
-gests = ('rock', 'paper', 'scissor')
+gests = ('rock', 'scissor', 'paper')
 
 def computer_random():
     rad = random.randrange(3)
-    return gests[rad]
+    return rad
 
 def user_win():
     print("User win!")
@@ -16,29 +16,30 @@ def computer_win():
 def draw():
     print("Draw!")
 
+def user_gest_number(x):
+    return gests.index(x)
+
+def who_win(user, comp):
+    if(user == 0 and comp == 2):
+        user_win()
+    elif(user == 2 and comp == 0):
+        computer_win()
+    elif(user<comp):
+        user_win()
+    elif(comp<user):
+        computer_win()
+    else:
+        draw()
+
 
 
 while(True):
     user_gest = input("Type rock, scissor or paper: ")
     computer_gest = computer_random()
-    print("Computer choose: ", computer_gest)
+    print("Computer choose: ", gests[computer_gest])
     print("...")
     time.sleep(2)
-    if(user_gest==gests[0] and computer_gest==gests[2]):
-        user_win()
-    elif(user_gest==gests[0] and computer_gest==gests[1]):
-        computer_win()
-    elif(user_gest==gests[0] and computer_gest==gests[0]):
-        draw()
-    elif(user_gest==gests[1] and computer_gest==gests[0]):
-        user_win()
-    elif(user_gest==gests[1] and computer_gest==gests[1]):
-        draw()        
-    elif(user_gest==gests[1] and computer_gest==gests[2]):
-        computer_win()
-    elif(user_gest==gests[2] and computer_gest==gests[0]):
-        computer_win()
-    elif(user_gest==gests[2] and computer_gest==gests[1]):
-        user_win()
-    elif(user_gest==gests[2] and computer_gest==gests[2]):
-        draw()
+    who_win(gests.index(user_gest), computer_gest)
+    time.sleep(2)
+    print("")
+    print("Let's play again!")
